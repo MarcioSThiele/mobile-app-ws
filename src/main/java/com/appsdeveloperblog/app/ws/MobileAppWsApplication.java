79289068e815d9dd04b9f53c8ab9b4719bd8dc7c
MarcioSThiele <marcio.sthiele@gmail.com>
2020-03-28 16:42:50 -0300

    adding simple

--- src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsApplication.java
+++ src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MobileAppWsApplication {
 
+	//this is the main application
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
 	}
